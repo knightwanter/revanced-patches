ba8c0e9c0ffcf186cc30bfd81cb12c0bd41f8122
Knight wanter <76393623+knightwanter@users.noreply.github.com>
2023-03-02 12:51:29 +0530

    just wondering

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/spoof/patch/ClientSpoofPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/spoof/patch/ClientSpoofPatch.kt
@@ -19,18 +19,16 @@ import org.jf.dexlib2.iface.instruction.FiveRegisterInstruction
 @Description("Spoofs the YouTube or Vanced client to prevent playback issues.")
 @ClientSpoofCompatibility
 @Version("0.0.2")
-class ClientSpoofPatch : BytecodePatch(
-    listOf(UserAgentHeaderBuilderFingerprint)
-) {
+class ClientSpoofPatch : BytecodePatch(listOf(UserAgentHeaderBuilderFingerprint)) {
+
     override fun execute(context: BytecodeContext): PatchResult {
-        val result = UserAgentHeaderBuilderFingerprint.result!!
-        val method = result.mutableMethod
+        val fingerprint = UserAgentHeaderBuilderFingerprint.result ?: return PatchResultSuccess()
 
-        val insertIndex = result.scanResult.patternScanResult!!.endIndex
-        val packageNameRegister = (method.instruction(insertIndex) as FiveRegisterInstruction).registerD
+        val method = fingerprint.mutableMethod
+        val insertIndex = fingerprint.scanResult.patternScanResult?.endIndex ?: return PatchResultSuccess()
 
-        val originalPackageName = "com.google.android.youtube"
-        method.addInstruction(insertIndex, "const-string v$packageNameRegister, \"$originalPackageName\"")
+        val packageNameRegister = (method.instruction(insertIndex) as? FiveRegisterInstruction)?.registerD ?: return PatchResultSuccess()
+        method.addInstruction(insertIndex, "const-string v$packageNameRegister, \"com.google.android.youtube\"")
 
         return PatchResultSuccess()
     }
